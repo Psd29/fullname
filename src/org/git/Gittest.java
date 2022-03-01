e403f63a696c89e8c754e83cffa0b319c454e901
Psd29 <prasathambika4433@gmail.com>
2022-03-01 14:00:45 +0530

    Name

---
+++ src/org/git/Gittest.java
@@ -0,0 +1,9 @@
+package org.git;
+
+public class Gittest {
+	public static void firstName() {
+		System.out.println("Prasath");
+
+	}
+
+}
---
+++ src/org/git/Gittest1.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class Gittest1 extends Gittest {
+	public static void lastName() {
+		System.out.println("Ambikapathy");
+
+	}
+public static void main(String[] args) {
+	firstName();
+	lastName();
+}
+}
